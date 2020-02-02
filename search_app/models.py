26a1e0499c6d08f2f805e7a61dd1e645acab5505
Venus <valievavm@gmail.com>
2020-02-02 11:37:33 +0100

    save data to db init commit

--- search_app/models.py
+++ search_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# model to save data to db
+class Search(models.Model):
+    from_location = models.CharField(max_length=100)
+    to_location = models.CharField(max_length=100)
+    search_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.from_location}-{self.to_location}"
+
